f12e0dd1b1a8327cd170dd2836ee23c7c144946f
V1XIONG <v1xiong@vplincsci020.ad.csbsju.edu>
2018-03-01 18:23:10 -0600

    Java Doc updates

--- SearchController.java
+++ SearchController.java
@@ -6,6 +6,7 @@ import java.util.*;
  * This class is the Database controller that allows a user to access the database and change things.
  *
  * First version- Ramon Angeles
+ * Java Doc -Vue Xiong
  * 
  * @author J. Ramon Angeles, James Heidelberger, Jaime Vega, Jonathan First, Vue Xiong
  * @version February 2018
@@ -14,25 +15,48 @@ import java.util.*;
 public class SearchController{
   
   private UniversityDBLibrary dblib;
-  String universityName;
-  private String state;
+  String universityName;//name of the university
+  private String state;//state where university is located
   private String locationType;//Urban, Rural,etc
   private String control;//whether school is private or public
-  private int numberOfStudents;
+  private int numberOfStudents;//number of students at a university
   private double female;//percentage of female students in the university
-  private double satVerbal;
-  private double satMath;
-  private double expenses;
-  private double financialAid;
-  private int numOfApplicants;
-  private double numAdmitted;
-  private double enrolled;
-  private int academicScale;
-  private int socialScale;
-  private int qualityOfLife;
+  private double satVerbal;//SAT verbal scores for a university
+  private double satMath;//SAT math scores for a university
+  private double expenses;//cost to attend a university 
+  private double financialAid;//financial aid a university offers
+  private int numOfApplicants;//number of students who apply to university
+  private double numAdmitted;//number of students admitted to a university
+  private double enrolled;//number of students enroll in a university
+  private int academicScale;//a university's academic standing
+  private int socialScale;//a university's social scale between 1-5
+  private int qualityOfLife;//quality of life on a university 1-5
   
-  public ArrayList<University> search(String universityName, String state, String location, String control, int numberOfStudents, double female, double SATVerbal,
-                                      double SATMath, double expenses, double financialAid, int numberOfApplicants, double admitted, double enrolled,
+  /**
+   * This method allows for a user to search for a university by passing/giving all the search citeria that they
+   * would like to search for. 
+   * 
+   * @param universityName the name of the university
+   * @param state where the university is located
+   * @param locationType whether the university is located in an rural, suburb, or city
+   * @param control whether the school is private or public
+   * @param numberOfStudents the number of students at a school
+   * @param female the percentage of female student on a campus
+   * @param satVerbal the sat verbal score of a university
+   * @param satMath the sat math score of a university
+   * @param expenses the cost of a university
+   * @param financialAid the financial aid that the school gives
+   * @param numberOfApplicants the number of students who apply to the university
+   * @param admitted the number of students admitted to the university
+   * @param enrolled the number of students enrolled at a university
+   * @param academcScale the academic scale of a university
+   * @param socialScale the social scale of a university
+   * @param qualityOfLifeScale the quality of life at a university
+   * 
+   * @returns an ArrayList of University
+   */
+  public ArrayList<University> search(String universityName, String state, String location, String control, int numberOfStudents, double female, double satVerbal,
+                                      double satMath, double expenses, double financialAid, int numberOfApplicants, double admitted, double enrolled,
                                       int academicScale, int socialScale, int qualityOfLifeScale){
     ArrayList<University> schoolInfo = new ArrayList<University>();
     String[][] school = dblib.university_getUniversities();
